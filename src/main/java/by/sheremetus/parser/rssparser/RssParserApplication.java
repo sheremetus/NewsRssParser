486e72e266eb752433714838a1d8ffba30825fec
kirja <kirjasheremet@gmail.com>
2024-08-24 16:02:13 +0300

    Initial commit

---
+++ src/main/java/by/sheremetus/parser/rssparser/RssParserApplication.java
@@ -0,0 +1,13 @@
+package by.sheremetus.parser.rssparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RssParserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/sheremetus/parser/rssparser/RssParserApplicationTests.java
@@ -0,0 +1,13 @@
+package by.sheremetus.parser.rssparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RssParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
